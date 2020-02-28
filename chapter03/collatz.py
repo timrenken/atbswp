b24d47c6d7e17c64475e9c2fc073583cfa2411ea
Tim Renken <tim@timrenken.io>
2020-02-27 21:09:53 -0600

    added exception on collatz project

--- chapter03/collatz.py
+++ chapter03/collatz.py
@@ -1,22 +1,23 @@
 import sys
 
-
 def collatz(number):
     global userNumber
     if number % 2 == 0:
         userNumber = number // 2
-
     elif number % 2 == 1:
         userNumber = 3 * number + 1
 
 userNumber = 0
 
-while True:
-    print('Enter number')
-    userNumber = int(input())
+try:
     while True:
-        print(userNumber)
-        if userNumber == 1:
-            sys.exit()
-        elif userNumber > 1:
-            collatz(userNumber)
+        print('Enter number')
+        userNumber = int(input())
+        while True:
+            print(userNumber)
+            if userNumber == 1:
+                sys.exit()
+            elif userNumber > 1:
+                collatz(userNumber)
+except ValueError:
+    print('Please enter an integer')
