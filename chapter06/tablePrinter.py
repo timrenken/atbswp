ccbca7fc226dc12be213dc06eab8f454ec2ddd36
Tim Renken <tim@timrenken.io>
2020-05-24 10:38:41 -0500

    completed Table Printer project

--- chapter06/tablePrinter.py
+++ chapter06/tablePrinter.py
@@ -1,6 +1,23 @@
-def printTable():
-
-
 tableData = [['apples','oranges','cherries','banana'],
              ['Alice','Bob','Carol','David'],
              ['dogs','cats','moose','goose']]
+
+colWidth = [0] * len(tableData)
+
+for x in range(len(tableData)):
+    for y in range(len(tableData[x])):
+        width = int(len(tableData[x][y]))
+        if width > colWidth[x]:
+            colWidth[x] = width
+
+def printTable():
+    for y in range(len(tableData[0])):
+        for x in range(len(tableData)):
+            print(tableData[x][y].rjust(colWidth[x]),end=' ')
+        print()
+
+printTable()        
+
+
+
+
