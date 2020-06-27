9624bbc3c852d24d670fbf2bb90a8497bd5adcb7
Tim Renken <tim@timrenken.io>
2020-06-27 13:34:05 -0700

    finished chapter 7 practice projects

--- chapter07/regexStripMethod.py
+++ chapter07/regexStripMethod.py
@@ -1,2 +1,22 @@
 #! python3
+import re
+
+def stripMethod(string,strip=None):
+    if strip == None:
+        stripLeft = re.compile(r'^\s*')
+        stripRight = re.compile(r'\s*$')
+    else:
+        stripLeft = re.compile(r'^['+re.escape(strip)+']*')
+        stripRight = re.compile(r'['+re.escape(strip)+']*$')
+    string = re.sub(stripLeft, "", string)
+    string = re.sub(stripRight, "", string)
+    return string
+
+
+print(stripMethod("   Hello world!   "))
+
+print(stripMethod('***Hello world! I am taking this over!***', '*'))
+
+
+
 
