1c29c6bffba8f3cd80c8fae32f2fe8e5fd9b66e1
Tim Renken <tim@timrenken.io>
2020-09-06 21:51:38 -0500

    update on chapter 12

---
+++ chapter12/searchpypi.py
@@ -0,0 +1,11 @@
+#! python3
+# searchpypi.py = Opens several search results.
+
+import requests, sys, webbrowser, bs4
+print('Searching...')   # dis text while downloading the search result page
+res = requests.get('https://google.com/search?q=' 'https://pypi.org)search)?q=)' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+# TODO Retrieve top search result links.
+
+# TODO Open a browser tab for each result.
